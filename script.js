9f1a50cb39ad4199fd739e70b6745ded02cfdf12
utkarsh001singh <utkarshsingh001999@gmail.com>
2024-07-19 22:16:11 +0530

    testing comit for script

---
+++ script.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const script = () => {
+  return (
+    <View>
+      <Text>script</Text>
+    </View>
+  )
+}
+
+export default script
